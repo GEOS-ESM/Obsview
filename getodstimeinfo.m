3d39b599cb8cda14e2a059f77a5086389e21d942
Wesley Davis <60855573+Wesley-J-Davis@users.noreply.github.com>
2025-04-18 09:55:20 -0400

    Update getodstimeinfo.m
    spacing and old code

--- getodstimeinfo.m
+++ getodstimeinfo.m
@@ -13,6 +13,7 @@ function [fjday,ljday,lhour,nsyn,varargout] = getodstimeinfo(odsfile)
 % 25Mar2002 Dick Dee (dee@dao.gsfc.nasa.gov)
 % 18Jan2005 - added NOBS
 % 15Apr2025 - updated to matlab 2024a
+
 if nargin==0, help(mfilename), return, end
 
 % first argument must be the name of a file:
@@ -23,51 +24,31 @@ end
 
 % open the file:
 
-%SD_id = hdfsd('start',odsfile,'read');
 SD_id = netcdf.open(odsfile);
-%if SD_id==-1, error(['Can''t open ' odsfile]); end
-% get time information:
-%idx = hdfsd('nametoindex',SD_id,'syn_beg');
 idx = netcdf.inqVarID(SD_id,'syn_beg');
-%sds_id = hdfsd('select',SD_id, idx);
-%[dname,rank,dimsizes,data_type,nattrs,status] = hdfsd('getinfo',sds_id);
+% [dname,data_type,dimids,nattrs] = netcdf.inqVar(SD_id,idx);
+% [dimname2, dimlen2] = netcdf.inqDim(SD_id,dimids(2));
+% [dimname1, dimlen1] = netcdf.inqDim(SD_id,dimids(1));
+% dimsizes = [dimlen2 dimlen1];
 [dname,data_type,dimsizes,nattrs] = netcdf.inqVar(SD_id,idx);
-%if status==-1, warning('Can''t read ''syn_beg''.'); end
 nsyn = dimsizes(2);
 
-%attr_index = hdfsd('findattr',sds_id,'first_julian_day');
-%[data,status] = hdfsd('readattr',sds_id,attr_index);
 data = netcdf.getAtt(SD_id,idx,'first_julian_day');
-%if status==-1, warning(['Can''t read ' attr_name '.']); end
 fjday = double(data);
 
-%attr_index = hdfsd('findattr',sds_id,'latest_julian_day');
 data = netcdf.getAtt(SD_id,idx,'latest_julian_day');
-%[data,status] = hdfsd('readattr',sds_id,attr_index);
-
-%if status==-1, warning(['Can''t read ' attr_name '.']); end
 ljday = double(data);
 
 data = netcdf.getAtt(SD_id,idx,'latest_synoptic_hour');
-%attr_index = hdfsd('findattr',sds_id,'latest_synoptic_hour');
-%[data,status] = hdfsd('readattr',sds_id,attr_index);
-
-%if status==-1, warning(['Can''t read ' attr_name '.']); end
 lhour = double(data);
+
 if nargout==5,
-    disp('entering getstidx')
     [idx,nob] = getstidx(SD_id);
-    disp('exiting getstidx')
     varargout(1) = {nob}
-
 end
-
-%hdfsd('endaccess',sds_id);
 
 % close the file:
 netcdf.close(SD_id);
-%status = hdfsd('end',SD_id);
-%if status==-1, error(['Can''t close ' odsfile]); end
 %--------------------------------------------------------------------
 
 function [idx,nobs] = getstidx(SD_id);
@@ -85,40 +66,18 @@ for name = {'syn_beg','syn_len'},
 
    dset_name = char(name);
    idx = netcdf.inqVarID(SD_id,dset_name);
-   %idx = hdfsd('nametoindex',SD_id,dset_name);
-   %sds_id = hdfsd('select',SD_id, idx);
-   %[dname,rank,dimsizes,data_type,nattrs,status] = hdfsd('getinfo',sds_id);
    [dname,data_type,dimids,nattrs] = netcdf.inqVar(SD_id,idx);
    [dimname2, dimlen2] = netcdf.inqDim(SD_id,dimids(2));
    [dimname1, dimlen1] = netcdf.inqDim(SD_id,dimids(1));
    dimsizes = [dimlen2 dimlen1];
 
-   % it's not dimsizes it's dimids
-   % need to pull dimids to get dimsizes
-%   if status==-1,
-%        hdfsd('end',SD_id);
-%        error(['Can''t read ' dset_name '.'])
-%   end
-
    if all(isfinite(dimsizes)) && length(dimsizes)==2,
 
-      start  = [0 0];
-      stride = [1 1];
-      edge   = dimsizes;
-      dname
-      data = netcdf.getVar(SD_id,idx); %,start,stride,edge)
-      %[data,status] = hdfsd('readdata',sds_id,start,stride,edge);
-      %if status==-1,
-      %   hdfsd('end',SD_id);
-      %   error(['Can''t read ' dset_name '.'])
-      %end
+      data = netcdf.getVar(SD_id,idx);
       collect = [collect double(data(:))];
-      %hdfsd('endaccess',sds_id);
 
    else
-      %hdfsd('end',SD_id);
       netcdf.close(SD_id);
-      error(['Unexpected dimensions of ' dset_name '.']);
 
    end
 
@@ -130,5 +89,4 @@ n = sum(nobs);       % total number of obs on file
 idx;
 i = find(idx<=n);    % remove out-of-bound indices:
 idx  = idx(i);
-%nobs = sum(nobs(i))
 nobs = nobs(i)
